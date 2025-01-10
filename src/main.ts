3b7419e6db4d53cc68e917532a0fccb3e1a2413d
21120190 <quocankg2611@gmail.com>
2025-01-11 02:05:14 +0700

    hotfix: please deploy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-import * as compression from 'compression'
-
-const PORT = parseInt(process.env.PORT, 10) || 3000
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({}))
   app.enableVersioning({ type: VersioningType.URI })
-  app.use(helmet())
-  app.use(compression())
-  await app.listen(PORT);
+  await app.listen(port);
 }
 bootstrap();
