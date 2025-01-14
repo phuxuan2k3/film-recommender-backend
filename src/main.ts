a1f786cb3eda1d214860175d784d0a0b3627406e
21120190 <quocankg2611@gmail.com>
2025-01-14 11:07:11 +0700

    add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { CatchEverythingFilter } from './common/ex-filter';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new CatchEverythingFilter(app.get(HttpAdapterHost)));
   app.enableVersioning({ type: VersioningType.URI })
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
